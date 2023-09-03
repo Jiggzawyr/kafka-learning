822a8ed46838957e4b453587a53d85daa4c19a2e
Jiggzawyr <martinstojanovic96@gmail.com>
2023-09-03 12:09:22 +0200

    first commit

---
+++ src/main/java/com/example/demo/KafkaListeners.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+    
+    @KafkaListener(
+        topics = "demo",
+        groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data);
+    }
+
+}
